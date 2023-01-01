4c1fb933f389fefb85a8a2615a7463a7eed3a9ea
xQualtrag <121761220+xQualtrag@users.noreply.github.com>
2023-01-01 23:27:03 +0600

    arrayCreator now supports array & string mode

--- arrayCreator.js
+++ arrayCreator.js
@@ -1,3 +1,8 @@
+console.log('1.String Mode')
+console.log('2.Array Mode')
+let menuSel = prompt('Please select a menu:');
+console.log(`Selected ${menuSel}`);
+if (menuSel === '1') {
 let customArr = []
 let arrLength = prompt("How many items do you want to add to the array?")
 for (i = 0; i < arrLength; i++) {
@@ -5,3 +10,13 @@ for (i = 0; i < arrLength; i++) {
   customArr.push(pushUserValue)
 }
 console.log(customArr)
+}
+if(menuSel === '2') {
+let customArr = []
+let arrLength = parseInt(prompt("How many items do you want to add to the array?"))
+for (i = 0; i < arrLength; i++) {
+  let pushUserValue = parseInt(prompt('Please enter a value to add to the array: '))
+  customArr.push(pushUserValue)
+}
+console.log(customArr)
+}
