4938a2a9a04ead52eca17d36577579baedcdcf7a
xQualtrag <121761220+xQualtrag@users.noreply.github.com>
2023-01-01 23:52:47 +0600

    dev changes #0.1.1

--- consoleCalculator.js
+++ consoleCalculator.js
@@ -1,1 +1,13 @@
+//Calculator Using Reduce() method
+
+//First of all create an empty array
+let storeNumber = [];
+//Now use a prompt() function to ask the user for a number
+//Make sure to parseInt()
+//After that we should code something that will ask the user for amount of number to be calculated
+let arrLength = parseInt(prompt("How many items do you want to add to the array?"));
+//Now use a prompt() function to ask the user for a number
+//Make sure to parseInt()
+let number = parseInt(prompt("Please enter a number: "));
+//After that we should code something that will ask us for the next number
 
